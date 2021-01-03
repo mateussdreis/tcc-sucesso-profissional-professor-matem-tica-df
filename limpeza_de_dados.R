6d1bfebbbb1eaf5f1f10c5b9c310df5ddd994d7e
mateussdreis <mateusreis@outlook.com.br>
2021-01-02 22:35:03 -0300

    Início do programa de interpretação dos dados do formulário enviado

---
+++ limpeza_de_dados.R
@@ -0,0 +1,15 @@
+
+#Limpando dados de idade etc
+
+library(tidyverse)
+library(dplyr)
+
+db <- read_csv("/home/mateus/Dropbox/TCC_mateus_reis/TCC/PROGRAMAÇÃO/tcc-sucesso-profissional-professor-matem-tica-df/Formulário - Pesquisa Sucesso Profissional.csv") %>% 
+  select(-c(`Carimbo de data/hora`,Gênero)) %>% 
+  mutate(
+    idade = {(Sys.Date() - `Data de nascimento`)/365.25}%>% 
+      as.numeric()#%>% 
+      #round(digits = 2)
+  )
+
+
