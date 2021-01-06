02b8f5108c0a9d69842480a5b818f24c2bd6f129
mateussdreis <mateusreis@outlook.com.br>
2021-01-06 18:29:52 -0300

    apaguei função anterior

--- limpeza_de_dados.R
+++ limpeza_de_dados.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(dplyr)
+library(tm)
 
 db <- read_csv("/home/mateus/Dropbox/TCC_mateus_reis/TCC/PROGRAMAÇÃO/tcc-sucesso-profissional-professor-matem-tica-df/Formulário - Pesquisa Sucesso Profissional.csv") %>% 
   select(-c(`Carimbo de data/hora`,Gênero)) %>% 
@@ -12,4 +13,21 @@ db <- read_csv("/home/mateus/Dropbox/TCC_mateus_reis/TCC/PROGRAMAÇÃO/tcc-suces
       #round(digits = 2)
   )
 
-#falta limpar o nome dos cabeçalhos
+#Limpando o nome dos cabeçalhos
+
+col.db<-colnames(db)
+
+col.db=col.db %>% 
+  removePunctuation() %>% 
+  gsub("Marque apenas um item por linha","",.) %>% 
+  gsub("Escolha apenas um item","",.)
+  
+#view(col.db)
+
+colnames(db)<-col.db
+
+#Excluindo a coluna do lattes e a coluna de e-mail:
+db<-db[,-c(12,51)]
+
+
+write_csv(db,"/home/mateus/Dropbox/TCC_mateus_reis/TCC/PROGRAMAÇÃO/tcc-sucesso-profissional-professor-matem-tica-df/base_tcc.csv")
--- word_clouds.R
+++ word_clouds.R
@@ -5,33 +5,6 @@ library(RWeka)
 library(lubridate)
 library(wordcloud)
 
-funcao_nuvem = function(a){
-    a %>% 
-    gsub("[^A-Za-z0-9]", " ", .) %>%   
-    stri_trans_general(., "Latin-ASCII") %>% 
-    iconv(., "UTF-8", "ASCII//TRANSLIT") %>% 
-    removePunctuation() %>% 
-    removeNumbers() %>%
-    tolower() %>% 
-    gsub("\n", "", ., fixed = T) %>% 
-    gsub(" ", " ", .) %>% 
-    removeWords(., stopwords("portuguese")) %>% 
-    gsub("\\s+", " ", .) %>% 
-    paste(., collapse = " ") %>% 
-    wordcloud()
-  
-}
-
-  #Dá pra melhorar essa worclous:   https://towardsdatascience.com/create-a-word-cloud-with-r-bde3e7422e8a ;  http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
-
-funcao_nuvem(db$`Me considero, ou não me considero, um professor de sucesso porque...`)
-
-funcao_nuvem(db$`Quais fatores você considera importante para ser bem sucedido?`)
-
-funcao_nuvem(db$`Comentários e/ou sugestões.`)
-
-###### Fazendo um Worcloud mais Elaborado
-
 
 #Função que gera um dataframe com a frequência das palavras
 
