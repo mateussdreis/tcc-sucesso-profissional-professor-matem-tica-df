8d03ae154fcd645387fe23f3f862ed06234da822
mateussdreis <mateusreis@outlook.com.br>
2021-01-12 10:55:45 -0300

    foi feito a correção da função nuvem adicionando um na.omit e gerei os gráficos.

--- word_clouds.R
+++ word_clouds.R
@@ -4,14 +4,16 @@ library(dplyr)
 library(RWeka)
 library(lubridate)
 library(wordcloud)
+library(tidyverse) 
 
-
+db<-read_csv("/home/mateus/Dropbox/TCC_mateus_reis/TCC/PROGRAMAÇÃO/tcc-sucesso-profissional-professor-matem-tica-df/base_tcc.csv")
+  
 #Função que gera um dataframe com a frequência das palavras
 
 freq.palavras=function(vetor){
   
   #coloca tudo num só vetor:
-  vetor=str_flatten(vetor,collapse = " ")
+  vetor=str_flatten(na.omit(vetor),collapse = " ")
   
   vetor=vetor %>% 
     #gsub("[^A-Za-z0-9]", " ", .) %>%   
@@ -47,30 +49,30 @@ funcao_nuvem2<-function(df){
 
 ## Me considero ou não me considero um professor de sucesso porque
 
+set.seed(123)
 funcao_nuvem2(freq.palavras(db$`Me considero ou não me considero um professor de sucesso porque`))
 
-freq.palavras(db$`Me considero ou não me considero um professor de sucesso porque`)
+Mecons.fre<-freq.palavras(db$`Me considero ou não me considero um professor de sucesso porque`)
 
 
 ##Quais fatores você considera importante para ser bem sucedido
-
+set.seed(123)
 funcao_nuvem2(freq.palavras(db$`Quais fatores você considera importante para ser bem sucedido`))
 
-freq.palavras(db$`Quais fatores você considera importante para ser bem sucedido`)
+Quais_fat.freq<-freq.palavras(db$`Quais fatores você considera importante para ser bem sucedido`)
 
 
 ## Comentários eou sugestões NÃO FUNCIONOU - VERIFICAR O OCORRIDO
-
+set.seed(123)
 funcao_nuvem2(freq.palavras(db$`Comentários eou sugestões`))
 
-freq.palavras(db$`Comentários eou sugestões`)
+Coment.freq<-freq.palavras(db$`Comentários eou sugestões`)
 
 
+#Vou salvar a frequência das palavras caso seja necessário de acordo com as orientações após fechar o questionário
 
 
-
-
-
+    
 
 
   
